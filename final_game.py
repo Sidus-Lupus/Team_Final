e882390e862864884a20f0f28f95a7b5e68c91ad
Sidus-Lupus <cannon.warren@ucas-edu.net>
2026-02-06 13:21:07 -0700

    added checkLegalMove

--- final_game.py
+++ final_game.py
@@ -1,14 +1,20 @@
 #CW AP final game project
 
-location1 = "X"
-location2 = "O"
-location3 = "X"
+location1 = " "
+location2 = " "
+location3 = " "
 location4 = "O"
 location5 = "X"
 location6 = "O"
 location7 = "X"
 location8 = "O"
 location9 = "X"
+
+def checkLegalMove(location):
+    if location == "X" or location == "O":
+        return False
+    else:
+        return True
 
 def printGameBoard(loc1, loc2, loc3, loc4, loc5, loc6, loc7, loc8, loc9 ):
     print(f"{loc1} | {loc2} | {loc3}")
